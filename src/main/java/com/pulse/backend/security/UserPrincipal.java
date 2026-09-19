e06253513e437783fc88080b974c1ab7208cadbb
DaliborKolesar <dalibor.kolesar@kosickaakademia.sk>
2026-09-19 20:56:22 +0200

    Add JWT service and Spring Security user details components

---
+++ src/main/java/com/pulse/backend/security/UserPrincipal.java
@@ -0,0 +1,46 @@
+package com.pulse.backend.security;
+import com.pulse.backend.PulseBackendApplication;
+import com.pulse.backend.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+   private final User user;
+
+   public User getUser(){
+       return user;
+   }
+   @Override
+    public Collection<?extends GrantedAuthority>getAuthorities(){
+       return List.of(new SimpleGrantedAuthority("Role_"+user.getRole().name()));
+   }
+   @Override
+    public String getPassword(){
+       return user.getPasswordHash();
+   }
+   @Override
+    public String getUsername(){
+       return user.getEmail();
+   }
+   @Override
+    public boolean isAccountNonExpired(){
+       return true;
+   }
+   @Override
+    public boolean isAccountNonLocked(){
+       return true;
+   }
+   @Override
+    public boolean isCredentialsNonExpired(){
+       return true;
+   }
+   @Override
+    public boolean isEnabled(){
+       return true;
+   }
+
+}
